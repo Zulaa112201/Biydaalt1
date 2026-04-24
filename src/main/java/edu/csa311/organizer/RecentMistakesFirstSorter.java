87100177af8c08b77cf58bb66b370bb943ae220e
Zulaa112201 <nominzulerdenebayrr@gmail.com>
2026-04-24 21:46:18 +0800

    Update RecentMistakesFirstSorter.java
    Removed commented-out FIFO sorting implementation and added a LIFO sorting function.

--- src/main/java/edu/csa311/organizer/RecentMistakesFirstSorter.java
+++ src/main/java/edu/csa311/organizer/RecentMistakesFirstSorter.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import edu.csa311.Card;
 
-// LIFO zarchmaar garah function
+
 public class RecentMistakesFirstSorter implements CardOrganizer {
     @Override
     public void organize(List<Card> cards) {
@@ -27,15 +27,3 @@ public class RecentMistakesFirstSorter implements CardOrganizer {
         cards.addAll(passed);
     }
 }
-// FIFO zarchmaar garah function
-// public class RecentMistakesFirstSorter implements CardOrganizer {
-//     @Override
-//     public void organize(List<Card> cards) {
-//         cards.sort((c1, c2) -> {
-//             if (c1.isLastAttemptFailed() == c2.isLastAttemptFailed()) {
-//                 return 0;
-//             }
-//             return c1.isLastAttemptFailed() ? -1 : 1; // c1 алдсан бол урагшаа
-//         });
-//     }
-// }
